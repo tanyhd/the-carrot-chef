aba3da0bd919d8ff1436b640690e16480c0f5c21
tanyhd <darren243@hotmail.com>
2022-10-05 19:40:34 +0800

    initial commit

---
+++ server/src/main/java/com/theCarrotChef/recipeSearch/RecipeSearchApplication.java
@@ -0,0 +1,13 @@
+package com.theCarrotChef.recipeSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeSearchApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/theCarrotChef/recipeSearch/RecipeSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theCarrotChef.recipeSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
